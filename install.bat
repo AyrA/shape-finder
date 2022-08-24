ad315847f0d7bda2ddbf4a92cf78294ddf396a60
Kevin Gut <kevin.gut@bitm.sg>
2022-08-24 21:37:00 +0200

    Make installation safer

--- install.bat
+++ install.bat
@@ -2,9 +2,17 @@
 SETLOCAL
 SET DEST=%APPTATA%\shapez.io\mods
 PUSHD "%~dp0"
+IF NOT EXIST findshape.js GOTO NOFILE
 CHOICE /C:YN /M "Install shape finder mod"
 IF ERRORLEVEL 2 GOTO END
+GOTO INSTALL
 
+:NOFILE
+ECHO Cannot find "%CD%\findshape.js"
+PAUSE
+GOTO END
+
+:INSTALL
 MD "%DEST%"
 COPY findshape.js "%DEST%" /Y
 ECHO Mod installed.
